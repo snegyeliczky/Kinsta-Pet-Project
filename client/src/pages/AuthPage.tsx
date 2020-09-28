bbf3063124bcd7f8674b463b5bffc92652b7ac2a
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-09-28 11:05:17 +0200

    switch added

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from "../components/authComp/Login";
 import Registration from "../components/authComp/Registration";
 import { Switch } from 'antd';
 
 const AuthPage = () => {
 
+    const [isReg, setReg] = useState<boolean>(true);
+
     return (
         <div>
-
             Hello Auth
             <Switch checkedChildren="Login" unCheckedChildren="Registration" defaultChecked onChange={e=>{
-                console.log(e)
+                setReg(e)
             }}/>
+            {isReg?<Login/>:<Registration/>}
 
-            <Login/>
-            <Registration/>
         </div>
     );
 };
