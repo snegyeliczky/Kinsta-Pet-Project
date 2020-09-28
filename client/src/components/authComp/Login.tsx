70007bc111df38e8005d0b1467dc021e0300d05d
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-09-28 12:24:47 +0200

    registration and login

--- client/src/components/authComp/Login.tsx
+++ client/src/components/authComp/Login.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import {Input} from 'antd';
-import { UserOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Button, Input} from 'antd';
+import { UserOutlined, EyeInvisibleOutlined, EyeTwoTone,CheckOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
+export const handleLogin=(username:string,password:string|number)=> {
+
+};
+
 const Login = () => {
+
+    const [username,setUsername] = useState<string>();
+    const [password,setPassword] = useState<string|number>()
+
+
     return (
         <div className={"authComp"}>
             <h3>Login</h3>
             <Input placeholder={"Username"} prefix={<UserOutlined />}/>
             <Input.Password placeholder={"PassWord"} iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
+            <Button shape={"round"} icon={<CheckOutlined/>} type={"primary"} onClick={event => {
+                username&&password?handleLogin(username,password):alert("missing parameter")}
+            }>Login</Button>
         </div>
     );
 };
--- client/src/components/authComp/Registration.tsx
+++ client/src/components/authComp/Registration.tsx
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import {Input,Button} from 'antd';
 import { MailOutlined,UserOutlined, EyeInvisibleOutlined, EyeTwoTone, CheckOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
+import {handleLogin} from "./Login";
 
 const Registration = () => {
 
@@ -9,11 +10,26 @@ const Registration = () => {
     let email = useRef<Input>(null);
     let password = useRef<Input>(null);
 
+    const registration =(newUsername:string,newEmail:string,newPassword:string|number):boolean=>{
+        try {
+            //backEnd registration comes here
+            console.log(newUsername,newPassword,newEmail);
+            return true;
+        }catch (e) {
+            return false;
+        }
+    };
+
     const handleRegistration= ():void => {
         let newUsername = username.current?username.current.state.value:undefined;
         let newEmail = email.current?email.current.state.value:undefined;
         let newPassword = password.current?password.current.state.value:undefined;
-        console.log(newUsername,newEmail,newPassword)
+        newUsername && newEmail && newPassword ?
+            registration(newUsername,newEmail,newPassword) ?
+                handleLogin(newUsername,newPassword) :
+                alert("registration failed")
+            : alert("missing parameter");
+
     };
 
     return (
@@ -22,7 +38,7 @@ const Registration = () => {
             <Input placeholder={"Username"} prefix={<UserOutlined />} ref={username}/>
             <Input placeholder={"E-mail"} prefix={<MailOutlined />} ref={email}/>
             <Input.Password ref={password} placeholder={"PassWord"} iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
-            <Button shape={"round"} icon={<CheckOutlined/>} type={"primary"} onClick={handleRegistration}>Submit</Button>
+            <Button shape={"round"} icon={<CheckOutlined/>} type={"primary"} onClick={handleRegistration}>Registration</Button>
         </div>
     );
 };
