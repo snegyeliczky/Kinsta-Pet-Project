b4a6e2ed6684a05823ce8af166fc1cf48ab89f08
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-10-07 15:48:34 +0200

    refactored Main page with collapse

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,15 +1,16 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {ApplicationContext} from "../context/ApplicationContext";
 import CompaniComponent from "../components/CompaniComponent";
 import "../../src/assets/MainStyle.css"
 import "../assets/ProjectAnimation.css"
 import {Collapse} from 'antd';
-import NewProjectModal from "../components/Modals/NewProjectModal";
+
 
 const MainPage = () => {
 
     const appContext = useContext(ApplicationContext);
     let userId: number = appContext.getUserId();
+    const [open,setOpen] = useState<string[]|string>();
 
 
     useEffect(() => {
@@ -17,14 +18,14 @@ const MainPage = () => {
     }, [userId]);
 
     function callback(key: string | string[]) {
-        console.log(key);
+        setOpen(key)
     }
 
     const {Panel} = Collapse;
 
 
     return (
-        <Collapse defaultActiveKey={['1']} onChange={callback} className={"company-container"}>
+        <Collapse defaultActiveKey={open} onChange={callback} className={"company-container"}>
             {
                 appContext.companies.map(company => {
                     return (
