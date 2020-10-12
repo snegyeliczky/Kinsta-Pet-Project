e3410cdc64743bb9ce048d1e60583c21b19abbe4
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-10-12 10:25:25 +0200

    rooter refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import MainPage from "./pages/MainPage";
 import {ApplicationProvider} from "./context/ApplicationContext";
-import {addHeaderAndFooter} from "./components/addHeaderAndFooter";
 import ProjectPage from "./pages/ProjectPage";
 import NewCompany from "./pages/NewCompany";
-import {useHistory} from "react-router-dom";
+
+import NavBar from "./parts/NavBar";
+import Footer from "./parts/Footer";
 
 
 function App() {
 
- const history = useHistory();
- console.log(history)
+    const style = {
+        justifyContent: "center",
+        width: "100%",
+        gridTemplateColumns: "10% 80% 10%",
+        background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 47%, rgba(0,212,255,1) 100%)",
+        height: "contentBox",
+        minHeight: "100vh",
+        paddingTop: "11Vh",
+        paddingBottom: "11vh"
+    };
+
 
     return (
         <Router>
             <ApplicationProvider>
                 <Route exact path={"/auth"} component={AuthPage}/>
-                <Route exact path={"/"} component={addHeaderAndFooter(MainPage)}/>
-                <Route exact path={"/project/:id"} component={addHeaderAndFooter(ProjectPage)}/>
-                <Route exact path={"/new-company"} component={addHeaderAndFooter(NewCompany)}/>
+
+                    <NavBar/>
+                    <div id={"main-content-container"} style={style}>
+                        <Switch>
+                            <Route exact path={"/"} component={MainPage}/>
+                            <Route exact path={"/project/:id"} component={ProjectPage}/>
+                            <Route exact path={"/new-company"} component={NewCompany}/>
+                        </Switch>
+                    </div>
+                    <Footer/>
+
             </ApplicationProvider>
         </Router>
 
