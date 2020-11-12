1a130f6afb745a0de0ba495eec17699e6109b655
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-11-12 18:49:31 +0100

    gql user1 test added

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,21 +4,8 @@ import User from "./model/User";
 import {Model} from "objection";
 import {resolvers} from "./resolvers/resolver";
 import {DbInit} from "./util/initialiser";
-import Task from "./model/Task";
-import UserStory from "./model/UserStory";
-import {GqlService} from "./services/GqlService";
-import Project from "./model/Project";
 import {GqlUtil} from "./util/GqlUtil";
-import ParticipateInvite from "./model/ParticipateInvite";
 
-
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers
-});
-
-let knex = require('knex')(knexConfig.production);
-Model.knex(knex);
 
 async function insertBaseUsersToDb() {
     let users = await User.query();
@@ -27,9 +14,21 @@ async function insertBaseUsersToDb() {
     }
     let inv = await GqlUtil.geTasksDistributionForProject(1);
     console.log(inv);
-}
+};
 
-server.start({port: 4001, endpoint: "/gql"}, () => {
-    console.log('Server is running on http://localhost:4001/gql');
-    insertBaseUsersToDb();
-});
+export const startServer =()=>{
+    const server = new GraphQLServer({
+        typeDefs: './src/schema.graphql',
+        resolvers
+    });
+
+    let knex = require('knex')(knexConfig.production);
+    Model.knex(knex);
+
+    server.start({port: 4001, endpoint: "/gql"}, () => {
+        console.log('Server is running on http://localhost:4001/gql');
+        insertBaseUsersToDb();
+    });
+};
+
+startServer();
--- server/src/test/register.test.ts
+++ server/src/test/register.test.ts
@@ -1,7 +1,25 @@
-function sum(a:number, b:number) {
+import {startServer} from "../app";
+import request from "graphql-request/dist";
+import User from "../model/User";
+
+function sum(a: number, b: number) {
     return a + b;
 }
 
+const getUserOne = `
+    query{
+    user(id:1){
+    id,firstName}
+    }
+    `
+
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test("Get user 1 from db", async () => {
+    const response = await request('http://localhost:4001/gql',getUserOne);
+    console.log(response);
+    expect(response.user.id).toEqual(1)
+
 });
