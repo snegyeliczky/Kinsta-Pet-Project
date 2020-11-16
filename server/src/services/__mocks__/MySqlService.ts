d43ae7a167311ef6cf561b110970f763e4188f42
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-11-16 14:01:33 +0100

    different test cases by change Ids

--- server/src/services/__mocks__/MySqlService.ts
+++ server/src/services/__mocks__/MySqlService.ts
@@ -8,22 +8,53 @@ export const MySqlService = {
     },
 
     getTasksForUserStory: (userStory: UserStory) => {
-        return [{id: 1, ready: 1}, {id: 2, ready: 1}];
+        if (userStory.id===1){
+            return [{id: 1, ready: 1}, {id: 2, ready: 1}];
+        }if (userStory.id===2){
+            return [{id: 3, ready: 0}, {id: 4, ready: 1}];
+        }else {
+            return [{id: 5, ready: 0}, {id: 6, ready: 0}];
+        }
+
     },
     getTasksForUser: (userId: number) => {
         return [{id: 1, ready: 0, title: "not ready"}, {id: 2, ready: 1, title: "ready"}]
     },
     getUserStoryForTask: (taskId: number) => {
-        return [
-            {
-                id: 1,
-                userId: 1,
-                projectId: 1,
-                userStory: 'frontend colors shames',
-                status: 0,
-                businessValue: null
-            }
-        ]
+        if (taskId===1 || taskId===2){
+            return [
+                {
+                    id: 1,
+                    userId: 1,
+                    projectId: 1,
+                    userStory: 'frontend colors shames',
+                    status: 0,
+                    businessValue: null
+                }
+            ]
+        }if (taskId===3||taskId===4){
+            return [
+                {
+                    id: 2,
+                    userId: 1,
+                    projectId: 1,
+                    userStory: 'frontend colors shames',
+                    status: 0,
+                    businessValue: null
+                }
+            ]
+        }else {
+            return [
+                {
+                    id: 3,
+                    userId: 1,
+                    projectId: 1,
+                    userStory: 'frontend colors shames',
+                    status: 0,
+                    businessValue: null
+                }
+            ]
+        }
 
     }
 
--- server/src/test/GqlUtil.test.ts
+++ server/src/test/GqlUtil.test.ts
@@ -1,9 +1,9 @@
 import {GqlUtil} from "../util/GqlUtil";
 jest.mock('../services/MySqlService');
 
-test("task distribution 2 finished, with mocked MySqlService for GqlUtil",  async () =>{
+test("task distribution 3 finished, with mocked MySqlService for GqlUtil",  async () =>{
     let taskDist =  await GqlUtil.geTasksDistributionForProject(1);
-    expect(taskDist.finishedTasks).toEqual(4)
+    expect(taskDist.finishedTasks).toEqual(3)
 });
 
 test(" unFinished Tasks id is 1 For User,  with mocked db", async () =>{
@@ -11,7 +11,17 @@ test(" unFinished Tasks id is 1 For User,  with mocked db", async () =>{
     expect(unFinishedTasks[0].id).toEqual(1)
 });
 
-test("user story status check return false", async () =>{
+test("user story status check 2 finished return true", async () =>{
     let data = await GqlUtil.checkUserStoryStatus(1);
     expect(data.status).toEqual(true);
-})
+});
+
+test("user story status check 1 finish 1 not return false", async () =>{
+    let data = await GqlUtil.checkUserStoryStatus(3);
+    expect(data.status).toEqual(false);
+});
+
+test("user story status 2 not finished return false", async () =>{
+    let data = await GqlUtil.checkUserStoryStatus(5);
+    expect(data.status).toEqual(false);
+});
