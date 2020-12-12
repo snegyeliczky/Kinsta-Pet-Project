9d9c7f88d70ee9b0736c231e0829dbff68196b6d
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-12-12 20:06:28 +0100

    User story Rerender problem ok -- refactor necessary

--- client/src/pages/ProjectPage.tsx
+++ client/src/pages/ProjectPage.tsx
@@ -16,7 +16,7 @@ import {getProjectsForCompanyByUser} from "../queries/companyQueries";
 import {ApplicationContext} from "../context/ApplicationContext";
 import InviteModal from "../components/Modals/InviteColaboratorModal";
 import {newUserStory} from "../queries/subscriptions";
-import {TaskModel} from "../Types/TaskModel";
+
 
 const ProjectPage = () => {
 
@@ -58,23 +58,22 @@ const ProjectPage = () => {
             //if not exist than push the user story to list (some = false)
             if (!some) newList.push(subscriptionData.data.newUserStory);
 
-
             // if userStory exist than update it (some = true)
             if (some) {
                 let updatedUserStory: UserStoryModel = subscriptionData.data.newUserStory;
-                newList = newList.map((us: UserStoryModel) => {
+               newList = newList.map((us: UserStoryModel) => {
                     if (us.id === updatedUserStory.id) {
-                        return Object.assign(updatedUserStory)
+                        return updatedUserStory
                     }
-                    return Object.assign(us)
+                    return us
                 });
             }
             console.log(newList);
-            return ({
+            return {
                 project: {
-                    userStories: [...newList]
+                    userStories: newList
                 }
-            })
+            }
         }
     });
 
@@ -131,7 +130,7 @@ const ProjectPage = () => {
         return (<Collapse>{storyList.map((userStory: UserStoryModel) => {
             return (
                 <CollapsePanel key={userStory.id}
-                               header={<UserStory UserStory={userStory} removeUserStory={removeUSerStoryById}/>}>
+                               header={<UserStory key={userStory.id} UserStory={{...userStory}} removeUserStory={removeUSerStoryById}/>}>
                     <TaskTable userStory={userStory}/>
                 </CollapsePanel>
             )
